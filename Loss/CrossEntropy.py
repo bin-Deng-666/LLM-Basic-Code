98f58290d4657034744ea304a545369a35add8c1
DengBin <db1124@buaa.edu.cn>
2025-08-29 17:13:40 +0800

    fix DPO loss bug

--- Loss/CrossEntropy.py
+++ Loss/CrossEntropy.py
@@ -10,7 +10,7 @@ class CrossEntropyLoss(nn.Module):
         exp_logits = torch.exp(logits)
         softmax_probs = exp_logits / torch.sum(exp_logits, dim=1, keepdim=True)
         # 获得每个样本的真实类别概率
-        true_class_probs = softmax_probs[torch.arrange(logits.shape[0]), labels]
+        true_class_probs = softmax_probs[torch.arange(logits.shape[0]), labels]
         # 计算对数概率
         log_probs = -torch.log(true_class_probs)
         # 求平均
