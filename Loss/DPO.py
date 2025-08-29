3cbb7199dc3e60723fbad2b9de79f2f23a01f2e1
Brin <76430441+bin-Deng-666@users.noreply.github.com>
2025-08-29 17:13:40 +0800

    fix DPO loss bug

--- Loss/DPO.py
+++ Loss/DPO.py
@@ -8,7 +8,10 @@ class DPOLoss(nn.Module):
         self.beta = beta
 
     def forward(self, policy_chosen_logprobs, policy_rejected_logprobs, ref_chosen_logprobs, ref_rejected_logprobs):
-        diff =(policy_chosen_logprobs - policy_rejected_logprobs) - (ref_chosen_logprobs - ref_rejected_logprobs)
+        log_chosen_ratio = policy_chosen_logprobs - ref_chosen_logprobs
+        log_rejected_ratio = policy_rejected_logprobs - ref_rejected_logprobs
+        
+        diff = log_chosen_ratio - log_rejected_ratio
         reward_diff = self.beta * diff
         dpo_loss = -F.logsigmoid(reward_diff).mean()
         return dpo_loss
